2961ee8be28a629b0e92ff07eb1eac850b9079a6
wenright <wenright@purdue.edu>
2015-11-14 14:43:28 -0500

    Added message for when there are no stops added

--- www/js/ctrl/StopsCtrl.js
+++ www/js/ctrl/StopsCtrl.js
@@ -1,7 +1,18 @@
 angular.module('controllers')
 
-.controller('StopsCtrl', function($scope, $localStorage, $rootScope, $state) {
+.controller('StopsCtrl', function($scope, $localStorage, $rootScope, $state, $ionicPopup) {
   $scope.myStops = $localStorage.getArray('myStops');
+
+  if (!($scope.myStops && $scope.myStops.length > 0)) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'Looks like you don\'t have any stops added',
+      template: 'Hit \'ok\' to head over to the browse tab where you can add some bus stops'
+    });
+
+    alertPopup.then(function(res) {
+      $state.go('app.browse');
+    });
+  }
 
   var currentState = $state.current;
 
